f04522e57c1fab1ef1c047fc306af74e93d523e7
Cindy Chen <xchen@lcs.on.ca>
2024-03-29 16:51:12 -0400

    Created detail view and organized project by creating Views group.

---
+++ Landmarks/Views/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Landmarks
+//
+//  Created by Cindy Chen on 2024-03-29.
+//
+
+import SwiftUI
+
+struct DetailView: View{
+    var body: some View{
+        Text("Hello, World!")
+    }
+}
+
+#Preview{
+    DetailView()
+}
