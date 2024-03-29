09a6dd621d394f999255fe1d4fafea808dbada83
Cindy Chen <xchen@lcs.on.ca>
2024-03-29 16:46:45 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Cindy Chen on 2024-03-29.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
